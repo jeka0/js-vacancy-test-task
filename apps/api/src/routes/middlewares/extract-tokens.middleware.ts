e5d1c1a14e52aac8097a1e0a10f9ec23e6055616
Latosh Yauheni <latoshyauheni@sqilsoft.by>
2024-04-07 18:01:02 +0300

    check

--- apps/api/src/routes/middlewares/extract-tokens.middleware.ts
+++ apps/api/src/routes/middlewares/extract-tokens.middleware.ts
@@ -3,9 +3,11 @@ import { COOKIES } from 'app-constants';
 
 const storeTokenToState = async (ctx: AppKoaContext, next: Next) => {
   let accessToken = ctx.cookies.get(COOKIES.ACCESS_TOKEN);
-
+  console.log(accessToken);
+  console.log(COOKIES.ACCESS_TOKEN);
   const { authorization } = ctx.headers;
-
+  console.log(ctx.headers);
+  console.log(authorization);
   if (!accessToken && authorization) {
     accessToken = authorization.replace('Bearer', '').trim();
   }
